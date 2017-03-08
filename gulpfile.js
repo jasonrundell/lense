3b018d84eddb2a5ca76cefcadc2e49a8e98bf826
jasonrundell <jason.rundell@gmail.com>
2017-03-07 19:37:40 -0500

    Wokring on more layout for bolierplater001

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('src/**/*.scss')
+    .pipe(autoprefixer({
+        cascade: false
+    }))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist'));
 });
