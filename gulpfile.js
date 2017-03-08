c6379c2996433a4dc787390550b2858c26a2ebf0
jasonrundell <jason.rundell@gmail.com>
2017-03-07 19:36:27 -0500

    Revert "Namespacing and introduction of flex"
    This reverts commit 435c49e83162d4188c5f4869572905e60eb25567.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,9 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('src/**/*.scss')
-    .pipe(autoprefixer({
-        cascade: false
-    }))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist'));
 });
